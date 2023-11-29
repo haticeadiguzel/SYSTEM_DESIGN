f3a1eeb43570822e354e05a487bb72521b538cb6
Hatice Adıgüzel <77553404+haticeadiguzel@users.noreply.github.com>
2023-11-29 20:25:22 +0300

    commit

--- api/api.py
+++ api/api.py
@@ -1,27 +1,13 @@
-from flask import request, jsonify, json, Flask
-from flask_sqlalchemy import SQLAlchemy
-from models.model import Thread, db
+from models.model import Thread, db, app, conn
+from flask import request, jsonify, json
 from flask_migrate import Migrate
 from rq import Queue
 import platform
-import redis
 import os
 
-app = Flask(__name__)
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "postgresql://hatice:ataturk@db_thread_container:5432/system"
-
-redis_host = "redis_thread_container"
-redis_port = 6379
-redis_db = 0
-
-conn = redis.Redis(host=redis_host, port=redis_port, db=redis_db)
 queue = Queue(connection=conn)
 
 migrate = Migrate(app, db)
-
-db.init_app(app)
 
 with app.app_context():
     db.create_all()
--- worker/worker.py
+++ worker/worker.py
@@ -1,24 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
+from models.model import Thread, db, app, conn
 from rq import Worker, Connection
-from flask import Flask
-from models.model import Thread, db
 import subprocess
-import redis
 import os
-
-app = Flask(__name__)
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "postgresql://hatice:ataturk@db_thread_container:5432/system"
-
-redis_host = "redis_thread_container"
-redis_port = 6379
-redis_db = 0
-
-conn = redis.Redis(host=redis_host, port=redis_port, db=redis_db)
-
-db.init_app(app)
-
 
 def run_command(command):
     try:
